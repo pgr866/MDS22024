c258a17df3fb4ddf9f14587504e1a8309dd8d17f
Pablo Gómez Rivas <114260059+pgr866@users.noreply.github.com>
2024-02-24 19:48:49 +0100

    Delete fornielesgomez directory

--- fornielesgomez/src/main/java/interfaz/Mostrar_valoracion_comentario.java
+++
@@ -1,5 +0,0 @@
-package interfaz;
-
-public class Mostrar_valoracion_comentario {
-	public Seccion_comentarios_item _seccion_comentarios;
-}
--- fornielesgomez/src/main/java/interfaz/Mostrar_valoracion_noticia.java
+++
@@ -1,5 +0,0 @@
-package interfaz;
-
-public class Mostrar_valoracion_noticia {
-	public Pagina_Noticia _pagina_Noticia;
-}
