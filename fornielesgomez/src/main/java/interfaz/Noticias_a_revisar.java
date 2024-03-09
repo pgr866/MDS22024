d024c4d1aea9a95fcd7ec135637166ca81e74c54
Pablo Gómez Rivas <114260059+pgr866@users.noreply.github.com>
2024-03-09 17:57:25 +0100

    Delete fornielesgomez directory

--- fornielesgomez/src/main/java/interfaz/Noticias_a_revisar.java
+++
@@ -1,8 +0,0 @@
-package interfaz;
-
-import java.util.Vector;
-
-public class Noticias_a_revisar {
-	public Editor _editor;
-	public Vector<Noticias_a_revisar_item> _item = new Vector<Noticias_a_revisar_item>();
-}
--- fornielesgomez/src/main/java/interfaz/Noticias_en_seccion_Identificado.java
+++
@@ -1,8 +0,0 @@
-package interfaz;
-
-import java.util.Vector;
-
-public class Noticias_en_seccion_Identificado {
-	public Explorar_secciones_Identificado _explorar_secciones_Identificado;
-	public Vector<Noticias_en_seccion_Identificado_item> _item = new Vector<Noticias_en_seccion_Identificado_item>();
-}
