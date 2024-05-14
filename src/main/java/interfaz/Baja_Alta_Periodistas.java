bdcbaa9f8ce29a825da4247eba49705a3ae6e3eb
Pablo Gomez Rivas <pgr866@inlumine.ual.es>
2024-05-14 19:49:59 +0200

    Commit

--- src/main/java/interfaz/Baja_Alta_Periodistas.java
+++ src/main/java/interfaz/Baja_Alta_Periodistas.java
@@ -5,6 +5,7 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 public class Baja_Alta_Periodistas extends vistas.VistaBajaaltaperiodistas {
 	
 //	private event _alta_periodista;
+//	private event _baja_periodista;
 //	private Label _nombre;
 //	private TextArea _introducir_nombre;
 //	private Label _apellidos;
@@ -29,6 +30,7 @@ public class Baja_Alta_Periodistas extends vistas.VistaBajaaltaperiodistas {
 		this._lista_periodistas = new Lista_periodistas(this);
 		Lista_periodistas();
 		this.getButtondaraltaperiodista().addClickListener(event->Alta_periodista());
+		this.getButtondarbajaperiodista().addClickListener(event->Baja_periodista());
 	}
 
 	public void Lista_periodistas() {
@@ -50,4 +52,18 @@ public class Baja_Alta_Periodistas extends vistas.VistaBajaaltaperiodistas {
 		nuevo_periodista.setUrl_foto_perfil("https://i.postimg.cc/m25GMKm4/foto.png");
 		this._editor.Baja_Alta_Periodistas(); // Refrescar pagina
 	}
+	
+	public void Baja_periodista() {
+		this._lista_periodistas.getComboboxlistaperiodistas().getValue();
+//		periodista.setEsEliminado(true);
+//		periodista.setContrasena("");
+//		periodista.setDni("");
+//		periodista.setEmail("");
+//		periodista.setFecha_nacimiento("");
+//		periodista.setTelefono(-1);
+//		periodista.setUrl_foto_perfil("");
+//		periodista.setDa_de_baja((basededatos.Editor) this._lista_periodistas._baja_Alta_Periodistas._editor.identificado);;
+		this._lista_periodistas._baja_Alta_Periodistas._editor.Baja_Alta_Periodistas(); // Refrescar pagina
+	}
+	
 }
--- src/main/java/interfaz/Lista_periodistas_item.java
+++ src/main/java/interfaz/Lista_periodistas_item.java
@@ -4,29 +4,13 @@ import vistas.VistaListaperiodistas_item;
 
 public class Lista_periodistas_item extends VistaListaperiodistas_item {
 	
-//	private event _baja_periodista;
 	public Lista_periodistas _lista_periodistas;
 	basededatos.Periodista periodista;
 	
 	public Lista_periodistas_item(Lista_periodistas _lista_periodistas, basededatos.Periodista periodista) {
 		super();
 		this._lista_periodistas = _lista_periodistas;
-		this._lista_periodistas._baja_Alta_Periodistas.getButtondarbajaperiodista().addClickListener(event->Baja_periodista());
 		this.periodista = periodista;
 		this.getLayoutlistaperiodistaitem().setText(periodista.getNombre() + " " + periodista.getApellidos());
-	}
-
-	public void Baja_periodista() {
-		if (this.getLayoutlistaperiodistaitem().getText() == this._lista_periodistas.getComboboxlistaperiodistas().getValue()) {
-			periodista.setEsEliminado(true);
-			periodista.setContrasena("");
-			periodista.setDni("");
-			periodista.setEmail("");
-			periodista.setFecha_nacimiento("");
-			periodista.setTelefono(-1);
-			periodista.setUrl_foto_perfil("");
-			periodista.setDa_de_baja((basededatos.Editor) this._lista_periodistas._baja_Alta_Periodistas._editor.identificado);;
-			this._lista_periodistas._baja_Alta_Periodistas._editor.Baja_Alta_Periodistas(); // Refrescar pagina
-		}
 	}
 }
