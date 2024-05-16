98c65c7de1605223de39928207b87934cd8d16f7
Pablo Gomez Rivas <pgr866@inlumine.ual.es>
2024-05-16 02:02:56 +0200

    Commit

--- src/main/java/interfaz/Lista_periodistas_item.java
+++ src/main/java/interfaz/Lista_periodistas_item.java
@@ -11,6 +11,6 @@ public class Lista_periodistas_item extends VistaListaperiodistas_item {
 		super();
 		this._lista_periodistas = _lista_periodistas;
 		this.periodista = periodista;
-		this.getLayoutlistaperiodistaitem().setText(periodista.getNombre() + " " + periodista.getApellidos());
+		this.getLayoutlistaperiodistaitem().setText(periodista.getNick_apodo());
 	}
 }
